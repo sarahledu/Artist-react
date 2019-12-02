23058d4ec50b92ef34cbde745492460941c3f345
sarah ledu <sarah.m.ledu@gmail.com>
2019-12-02 16:09:40 +0100

    ...

--- client/src/views/ManageArtists.jsx
+++ client/src/views/ManageArtists.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import APIHandler from "./../api/handler";
+const handler = new APIHandler();
+console.log("handler=>", handler);
 
 export default function ManageArtists() {
+    const [artists, setArtists] =useState([]);
+
+
+    useEffect(()=> {
+        handler
+          .get("/manage-artists")
+          .then(apiRes => {
+            console.log("here", apiRes);
+            setArtists(apiRes.data)
+            
+          })
+          .catch(apiErr => console.error(apiErr));
+      },[])
+    
+    
     return (
         <div>
             <h1>Manage Artists</h1>
+            <span>{}</span>
+    
         </div>
     )
 }
