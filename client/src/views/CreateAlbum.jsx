0cedb30de3efc582989d6a0dd6ff7fa6bb474756
sarah ledu <sarah.m.ledu@gmail.com>
2019-12-02 21:57:06 +0100

    create artist en cours

--- client/src/views/CreateAlbum.jsx
+++ client/src/views/CreateAlbum.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
 export default function CreateAlbum() {
-  return (
-    <div>
-      <h1>Create Album</h1>
-    </div>
-  );
+    return (
+        <div>
+            
+        </div>
+    )
 }
--- client/src/views/CreateArtist.jsx
+++ client/src/views/CreateArtist.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import APIHandler from "../api/handler";
+const handler = new APIHandler();
+console.log("handler=>", handler);
 
-export default function CreateArtist() {
-    return (
-        <div>
-            <h1>Create Artist</h1>
-        </div>
-    )
-}
+const CreateArtist = () => {
+  const [artist, setArtist] = useState({ isBand: false });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("in the submit", artist);
+    const formData = new FormData();
+    for (let key in artist) {
+      console.log(key);
+      formData.append(key, artist[key]);
+    }
+    console.log(formData);
+    handler
+      .post("/create-artist", formData)
+      .then(apiRes => {
+        console.log(apiRes);
+      })
+      .catch(apiErr => console.log(apiErr));
+  }
+
+  function handleChange(e) {
+    const key = e.target.name;
+    const value = e.target.value;
+
+    if (e.target.type === "checkbox") {
+      const checkedBoxValue = e.target.checked;
+
+      setArtist({ ...artist, isBand: checkedBoxValue });
+    } else {
+      setArtist({ ...artist, [key]: value });
+    }
+  }
+
+  return (
+    <div>
+      <h1>Create Artist</h1>
+      <form onChange={handleChange} onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" name="name" />
+        <label htmlFor="style">Style</label>
+        <input id="style" type="text" name="style" />
+        <label htmlFor="description">Description</label>
+        <input id="description" type="text" name="description" />
+        <label htmlFor="isBand">Band</label>
+        <input id="isBand" type="checkbox" name="isBand" value="true" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateArtist;
